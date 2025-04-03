36d8bdf0552025bb4c97e250f87cf642c28f7810
IKoshelev <koshelew@live.com>
2025-04-03 02:42:20 +0200

    Installed full bootstrap

--- DemoPWA/Program.cs
+++ DemoPWA/Program.cs
@@ -13,6 +13,8 @@ namespace DemoPWA
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddBlazorBootstrap();
+
             await builder.Build().RunAsync();
         }
     }
